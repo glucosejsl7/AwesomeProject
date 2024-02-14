a18b3658a185816b5403ce2926df5416fcbe374a
glucosejsl7 <122554576+glucosejsl7@users.noreply.github.com>
2024-02-14 16:16:50 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Extra2 from "./screens/Extra2";
+
+export default function App() {
+  return (
+    <Extra2/>
+  );
+}
